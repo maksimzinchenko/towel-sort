37b357bc1719ef9beb3c3c85866d1ea6966914fd
lipkij <lipkij@gmail.com>
2023-01-17 11:12:06 +0400

    Solution with reduce and reverse flag

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    // Check matrix for undefined. 
+    // We can also change default params of function but don't touch them for interface consistens.
+    matrix = matrix || [];
+    return matrix.reduce((plane, row, index) => {
+        return plane.concat(!(index % 2)? row : row.reverse());
+    },[]);
 }
